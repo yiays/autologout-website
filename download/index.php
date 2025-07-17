ceb0483bcbdd5add6015071608b4b63ef4a19379
Yiays <yiays@yiays.com>
2025-07-18 00:47:59 +1200

    Update website for stable release of the desktop app

--- download/index.php
+++ download/index.php
@@ -9,6 +9,41 @@ include('../includes/header.php');
 <div class="autohide-panel" id="stable">
   <a href="#stable" class="panel-header autohide-show">
     <span>&gt;</span>
+    Install AutoLogout Stable
+  </a>
+  <a href="#" class="panel-header autohide-hide">
+    <span>&darr;</span>
+    Install AutoLogout
+  </a>
+  <div class="panel-content">
+    <strong>Installer</strong>
+    <p>
+      Use the guided installer to easily install AutoLogout. You only need to install AutoLogout once
+      per computer. To set it up in each account, open AutoLogout from the start menu and enter a
+      supervisor password.
+    </p>
+    <a class="btn" href="https://github.com/yiays/AutoLogout/releases/latest/download/AutoLogoutSetup.exe" target="_blank">
+      Download from GitHub
+    </a>
+    <br>
+    <strong>Portable</strong>
+    <p>
+      With a portable installation, you will need to find a place to keep the application. You might
+      also want to change file permissions to prevent tampering.
+    </p>
+    <a class="btn" href="https://github.com/yiays/AutoLogout/releases/latest/download/AutoLogout-Portable.zip" target="_blank">
+      Download from GitHub
+    </a>
+    <br>
+    <a class="btn secondary" href="https://github.com/yiays/AutoLogout" target="_blank">
+      View source code
+    </a>
+  </div>
+</div>
+
+<div class="autohide-panel" id="preview">
+  <a href="#preview" class="panel-header autohide-show">
+    <span>&gt;</span>
     Install AutoLogout Preview
   </a>
   <a href="#" class="panel-header autohide-hide">
@@ -16,13 +51,12 @@ include('../includes/header.php');
     Install AutoLogout Preview
   </a>
   <div class="panel-content">
-    <strong>Portable</strong>
+    <strong>Pre-release</strong>
     <p>
-      With a portable installation, you will need to find a place to keep the application. You will
-      also need to add a startup shortcut yourself.
+      Browse the GitHub releases tab for information about the latest releases and pre-releases.
     </p>
-    <a class="btn" href="https://github.com/yiays/AutoLogout/releases/latest" target="_blank">
-      Download from GitHub
+    <a class="btn" href="https://github.com/yiays/AutoLogout/releases" target="_blank">
+      GitHub Releases
     </a>
     <br>
     <a class="btn secondary" href="https://github.com/yiays/AutoLogout" target="_blank">
@@ -44,16 +78,6 @@ include('../includes/header.php');
     style="box-shadow: none"
   >
 </div>
-
-<script type="text/javascript">
-  // If the url is extended at all, attempt to redirect to the AutoLogout Manager app
-  if (window.location.pathname.length > 5 || window.location.search) {
-    // Construct the deep link URL
-    const deepLinkUrl = `autologoutmanager://${window.location.pathname.slice(5)}${window.location.search}`;
-    // Redirect to the deep link
-    window.location.href = deepLinkUrl;
-  }
-</script>
 <?php
 include('../includes/footer.php')
 ?>
